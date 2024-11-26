9de96c0e9aa913c7285d92d60543639a38a3bd0a
Ben Zion Fischer <benzionfisher@gmail.com>
2024-11-26 18:12:04 +0200

    add convertion to google coral accelerator

--- convert.py
+++ convert.py
@@ -1,9 +1,9 @@
 from ultralytics import YOLO
 
-# Load the YOLO11 model
-# model = YOLO("yolo11n.pt")
-
 model = YOLO("yolov8n.pt")
 
 # Export the model to TFLite format
-model.export(format="tflite")  # creates 'yolo11n_float32.tflite'
+model.export(format="tflite", imgsz=320, int8=True)
+
+# after convertion to quantized tflite format, compile it for google coral:
+# https://colab.research.google.com/github/google-coral/tutorials/blob/master/compile_for_edgetpu.ipynb?authuser=1#scrollTo=itv0Kj7N6ALw
