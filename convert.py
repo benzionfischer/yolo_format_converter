8dde75a7edac55a5fb49214071373d6a4f797002
Ben Zion Fischer <benzionfisher@gmail.com>
2024-11-27 15:17:18 +0200

    ..

--- convert.py
+++ convert.py
@@ -7,3 +7,7 @@ model.export(format="tflite", int8=True, imgsz=320)
 
 # after convertion to quantized tflite format, compile it for google coral:
 # https://colab.research.google.com/github/google-coral/tutorials/blob/master/compile_for_edgetpu.ipynb?authuser=1#scrollTo=itv0Kj7N6ALw
+
+# add to the notebook
+# pip install ultralytics
+# !yolo export model=yolo11n_custom_200_epoches_CPU_510_images_imgsz_320.pt format=edgetpu
