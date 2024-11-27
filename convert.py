d1e98e86e17687199cf781ddfed51fa5272e8a6c
Ben Zion Fischer <benzionfisher@gmail.com>
2024-11-27 15:05:40 +0200

    ..

--- convert.py
+++ convert.py
@@ -1,9 +1,9 @@
 from ultralytics import YOLO
 
-model = YOLO("yolov8n_custom_200_epoches_CPU_510_images.pt")
+model = YOLO("yolo11n_custom_200_epoches_CPU_510_images_imgsz_320.pt")
 
 # Export the model to TFLite format
-model.export(format="tflite", imgsz=240, int8=True)
+model.export(format="tflite", int8=True, imgsz=320)
 
 # after convertion to quantized tflite format, compile it for google coral:
 # https://colab.research.google.com/github/google-coral/tutorials/blob/master/compile_for_edgetpu.ipynb?authuser=1#scrollTo=itv0Kj7N6ALw
